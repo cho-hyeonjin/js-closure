91269749675eea774f6898da89dfdd25159a0d0d
cho-hyeonjin <gusls2005@gmail.com>
2024-02-28 23:42:36 +0900

    feat: 객체 리터럴을 리턴하는 함수

--- createCounter.js
+++ createCounter.js
@@ -2,23 +2,16 @@
 // 열린 람다식 내부의 자유 변슈를 묶인 변수로 만듦으로써
 // 열린 람다식을 닫힌 람다식으로 만들어 주는 것
 // ------------------------------------------------------------------------------
+// 함수를 리턴하는 함수는 언제나 클로저를 형성한다.
+// ------------------------------------------------------------------------------
 
-// 콜백 함수로 클로저 구현하기
-function createCounter(callback) {
+// TODO : 객체 메서드로 클로저 구현하기
+// 열린 람다식, 함수를 리턴하는 함수 createCounter
+function createCounter() {
+  // createCounter 함수의 내부 변수 count (현재는 자유 변수 상태)
   let count = 0;
   console.log(count, "나는 상태를 유지합니다.");
 
-  return function () {
-    count += 1; // ✅
-
-    callback(count);
-  };
+  // 리턴값으로 (함수가 아닌) 객체 리터럴 설정
+  return {};
 }
-
-const myCounter = createCounter(function (currentCount) {
-  console.log("현재 카운트:", currentCount);
-});
-
-myCounter();
-myCounter();
-myCounter();
