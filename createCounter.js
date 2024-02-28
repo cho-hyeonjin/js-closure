e6a416d417a18b8cf259d28b8678346eee415840
cho-hyeonjin <gusls2005@gmail.com>
2024-02-29 00:12:49 +0900

    feat: 객체 메서드로 구현된 클로저를 이용하여 찍어낸 인스턴스에서 클로저 메서드 이용하여 set/get

--- createCounter.js
+++ createCounter.js
@@ -6,23 +6,33 @@
 // ------------------------------------------------------------------------------
 
 // TODO : 객체 메서드로 클로저 구현하기
-// 열린 람다식, 함수를 리턴하는 함수 createCounter
+// 열린 람다식, 함수를 리턴하는 함수 createCounter (클로저를 리턴하는 함수)
 function createCounter() {
-  // createCounter 함수의 내부 변수 count (현재는 자유 변수 상태)
+  // 클로저를 반환하는 함수의 지역 변수 count (현재는 자유 변수 상태)
   let count = 0;
   console.log(count, "나는 상태를 유지합니다.");
 
   // 리턴값으로 (함수가 아닌) 객체 리터럴 설정
   return {
-    // count를 증가시키는 메서드 선언
+    // count를 증가시키는 메서드 increment 선언 (setter) --- 클로저(를 형성하는 메서드)
     increment: function () {
       // 외부 환경(Lexical Environment)과 묶이는 코드, count 변수가 묶인 변수가 되는 순간
       count += 1;
       return count;
     },
-    // 현재 count 값을 확일할 수 있는 메서드 선언
+    // 현재 count 값을 확일할 수 있는 메서드 선언 (getter) --- 클로저(를 형성하는 메서드)
     getCurrentCount: function () {
       return count;
     },
+    // increment 메서드와 getCurrentCount 메서드는 클로저(를 형성하는) 메서드
   };
 }
+
+// 클로저를 리턴하는 함수 createCounter 의 실행 결과가 할당된 상수 myCounter
+// 즉, myCounter 함수는 클로저 함수.
+const myCounter = createCounter();
+// myCounter 객체의 increment 메서드 (setter) 를 호출해서 count 값 증가
+myCounter.increment(); // count = 1
+myCounter.increment(); // count = 2
+// console.log()의 파라미터로 myCounter 객체의 getCurrentCount 메서드 (getter) 호출 결과를 전달하여 현재의 count 값 확인
+console.log(myCounter.getCurrentCount()); // 2
