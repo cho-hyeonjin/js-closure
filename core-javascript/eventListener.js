db3ef2428b54def7fedf5d3abf823f9e8b018697
cho-hyeonjin <gusls2005@gmail.com>
2024-03-04 21:17:32 +0900

    refactor: 이벤트 리스너의 첫번째 인자에 이벤트 객체가 주입되지 않도록 bind 메서드로 null 바인딩

--- core-javascript/eventListener.js
+++ core-javascript/eventListener.js
@@ -1,9 +1,6 @@
 var flowers = ["🌸", "🌼", "🌺"];
 var $ul = document.createElement("ul");
 
-// $li 의 click 이벤트 리스너의 콜백 함수로 전달하던 익명 함수가 다른 곳에서도 반복적으로 쓰이는 코드라면?
-// → 공통 함수로 쓸 수 있게 외부로 분리
-//   (분리한 함수를 alertFlower 변수에 할당해줬다.)
 var alertFlower = function (flower) {
   alert("your choice is " + flower);
 };
@@ -11,12 +8,8 @@ var alertFlower = function (flower) {
 flowers.forEach(function (flower) {
   var $li = document.createElement("li");
   $li.innerText = flower;
-  // $li 의 click 이벤트 리스너의 콜백 함수로 외부 함수 전달 → 클로저 형성
-  // 단, 여기서 문제는 각각의 li를 클릭하면 "🌸", "🌼", "🌺"이 아닌 [object MouseEvent]라는 값이 출력된다.
-  // 왜?..
-  // 아래 코드에서 콜백으로 전달한 외부 함수 alertFlower 함수의 인자에 대한 제어권을 addEventListener가 가진 상태.
-  // addEventListener 는 콜백 함수를 호출할 때 첫 번쨰 인자에 'Event' 객체를 주입하기 때문.
-  $li.addEventListener("click", alertFlower);
+  // bind 메서드로 첫번째 인자를 null로 명시적으로 변경한다. (그러나 이렇게 되면 this에 null이 바인딩된다는 점과 이벤트 객체가 인자로 넘어오는 순서가 바뀐다는 점을 감안해야 한다.)
+  $li.addEventListener("click", alertFlower.bind(null, flower));
   $ul.appendChild($li);
 });
 
